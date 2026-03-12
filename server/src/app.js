dc6852da68ead68099f16f29252f0bd419ad9e33
Avis Ejerenwa <ejerenwaavis.com>
2026-03-12 02:13:44 -0400

    added conole.err

--- server/src/app.js
+++ server/src/app.js
@@ -86,5 +86,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  console.error(`[${new Date().toISOString()}] Starting Rokit Media API...`);
   process.stderr.write(`[${new Date().toISOString()}] 🚀 Rokit Media API running on port ${PORT}\n`);
 });
